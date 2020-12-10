97aaf2f9a886f74fc9d49aad87eadc8ce0cd5159
Riyaad Azad <azad.riyaad@gmail.com>
2020-12-10 20:08:49 +0000

    Create file

---
+++ a113.py
@@ -0,0 +1,12 @@
+#   a113_forward_and_right.py
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+# Add code to make the turtle move forward 20 pixels
+painter.forward(20)
+# and then turn right 20 pixels
+painter.right(20)
+
+wn = trtl.Screen()
+wn.mainloop()
